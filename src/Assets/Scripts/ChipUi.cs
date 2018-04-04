51f511b281376dac7ca6848b736a0cdbc9c272f3
TripleWhy <TripleWhy@users.noreply.github.com>
2018-04-04 18:39:22 +0200

    Implemented chip dragging
    Man, that was cumbersome.

--- src/Assets/Scripts/ChipUi.cs
+++ src/Assets/Scripts/ChipUi.cs
@@ -4,10 +4,11 @@
 	using System.Collections.Generic;
 	using UnityEngine;
 	using UnityEngine.UI;
+	using UnityEngine.EventSystems;
 
-	public class ChipUi : MonoBehaviour
+	public class ChipUi : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 	{
-		private static Color[] portColors =
+		private static readonly Color[] portColors =
 			{
 				new Color(0.83F, 0.20F, 0.20F, 1.00F),
 				new Color(0.31F, 0.68F, 0.24F, 1.00F),
@@ -28,25 +29,41 @@
 		public int inPortCount = 1;
 		public int outPortCount = 1;
 		public bool hasReset = true;
+		[HideInInspector]
+		public bool skipSetup = false;
 
 		private GameObject[] inPorts;
 		private GameObject[] outPorts;
 
+		private Vector3 pointerWorldOffset;
+		private RectTransform rectTransform;
+		private Canvas canvas;
+		private RectTransform canvasRectTransform;
+		private ChipUi draggingInstance;
+
 		void Awake ()
 		{
-			int maxTotalPortCount = Mathf.Max(inPortCount, outPortCount) + (hasReset ? 1 : 0);
-			bool useCompactFormat = false;
-			if (maxTotalPortCount >= 4)
+			rectTransform = (RectTransform)transform;
+			canvas = GetComponentInParent<Canvas>();
+			canvasRectTransform = (RectTransform)canvas.transform;
+
+			if (!skipSetup)
 			{
-				useCompactFormat = true;
-				RectTransform tr0 = (RectTransform)in0.transform;
-				RectTransform rectTr = (RectTransform)transform;
-				rectTr.sizeDelta = new Vector2(rectTr.sizeDelta.x, ((3 * maxTotalPortCount - 1) * tr0.sizeDelta.y) / 2);
+				skipSetup = true;
+				int maxTotalPortCount = Mathf.Max(inPortCount, outPortCount) + (hasReset ? 1 : 0);
+				bool useCompactFormat = false;
+				if (maxTotalPortCount >= 4)
+				{
+					useCompactFormat = true;
+					RectTransform tr0 = (RectTransform)in0.transform;
+					RectTransform rectTr = (RectTransform)transform;
+					rectTr.sizeDelta = new Vector2(rectTr.sizeDelta.x, ((3 * maxTotalPortCount - 1) * tr0.sizeDelta.y) / 2);
+				}
+				setupPorts(inPortCount, useCompactFormat, ref in0, ref inR, ref inPorts);
+				setupPorts(outPortCount, useCompactFormat, ref out0, ref outR, ref outPorts);
+				if (sprite != null)
+					icon.sprite = sprite;
 			}
-			setupPorts(inPortCount, useCompactFormat, ref in0, ref inR, ref inPorts);
-			setupPorts(outPortCount, useCompactFormat, ref out0, ref outR, ref outPorts);
-			if (sprite != null)
-				icon.sprite = sprite;
 		}
 
 		private void setupPorts(int portCount, bool useCompactFormat, ref GameObject port0, ref GameObject resetPort, ref GameObject[] ports)
@@ -85,6 +102,61 @@
 			port0 = null;
 			resetPort = null;
 		}
+
+		#region IPointerDownHandler implementation
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			Vector2 n_originalLocalPointerPosition;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out n_originalLocalPointerPosition);
+			pointerWorldOffset = rectTransform.InverseTransformPoint(n_originalLocalPointerPosition) - rectTransform.InverseTransformPoint(new Vector3());
+		}
+
+		#endregion
+
+		#region IBeginDragHandler implementation
+
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			if (draggingInstance != null)
+				return;
+			if (canvas.tag != "Sidebar")
+				return;
+			draggingInstance = Instantiate(this, canvasRectTransform);
+		}
+
+		#endregion
+
+		#region IDragHandler implementation
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			ChipUi chip = draggingInstance ?? this;
+
+			if (chip.rectTransform == null || chip.canvasRectTransform == null)
+				return;
+
+			Vector2 worldPosition = eventData.position;
+			if (eventData.pressEventCamera != null)
+				worldPosition = eventData.pressEventCamera.ScreenToWorldPoint(eventData.position);
+
+			chip.rectTransform.position = (Vector3)(worldPosition) - pointerWorldOffset;
+		}
+
+		#endregion
+
+		#region IEndDragHandler implementation
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			if (draggingInstance != null)
+			{
+				Destroy(draggingInstance.gameObject);
+				draggingInstance = null;
+			}
+		}
+
+		#endregion
 	}
 
 }
---
+++ src/Assets/Scripts/PortUi.cs
@@ -0,0 +1,42 @@
+﻿namespace AssemblyCSharp
+{
+	using UnityEngine;
+	using UnityEngine.EventSystems;
+
+	public class PortUi : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+	{
+		#region IPointerDownHandler implementation
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			print("PortUi.OnPointerDown");
+		}
+		#endregion
+
+		#region IBeginDragHandler implementation
+
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			print("PortUi.OnBeginDrag");
+		}
+
+		#endregion
+
+		#region IDragHandler implementation
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			print("PortUi.OnDrag");
+		}
+
+		#endregion
+
+		#region IEndDragHandler implementation
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			print("PortUi.OnEndDrag");
+		}
+
+		#endregion
+	}
+}
