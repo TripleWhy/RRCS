ee3e604a248b9b065e523927d752618863201ecf
TripleWhy <TripleWhy@users.noreply.github.com>
2018-04-04 21:48:44 +0200

    Enabled mouse wheel zooming

--- src/Assets/Scripts/CameraControls.cs
+++ src/Assets/Scripts/CameraControls.cs
@@ -19,7 +19,7 @@
 			if (Screen.height != screenHeight)
 			{
 				screenHeight = Screen.height;
-				cam.orthographicSize = inverseZoom * screenHeight * 0.5f;
+				UpdateCameraZoom();
 			}
 
 			if (Input.GetMouseButton(2))
@@ -27,6 +27,18 @@
 				float speed = cam.orthographicSize;
 				transform.position -= new Vector3(speed * Input.GetAxis("Mouse X") * Time.deltaTime, speed * Input.GetAxis("Mouse Y") * Time.deltaTime, 0f);
 			}
+
+			float wheel = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime;
+			if (!Mathf.Approximately(wheel, 0f))
+			{
+				inverseZoom *= Mathf.Exp(-10 * wheel);
+				UpdateCameraZoom();
+			}
+		}
+
+		private void UpdateCameraZoom()
+		{
+			cam.orthographicSize = inverseZoom * screenHeight * 0.5f;
 		}
 	}
 }
