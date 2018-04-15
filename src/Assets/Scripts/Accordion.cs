4e2cfd7b9f98d4b0331f41d588bc576289a0e675
TripleWhy <TripleWhy@users.noreply.github.com>
2018-04-15 11:19:49 +0200

    Implemented Accordion

---
+++ src/Assets/Scripts/Accordion.cs
@@ -0,0 +1,26 @@
+﻿namespace AssemblyCSharp
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class Accordion : MonoBehaviour
+	{
+		public AccordionSection[] sections;
+
+		// Use this for initialization
+		void Start()
+		{
+			foreach (AccordionSection section in sections)
+				section.sectionButton.onClick.AddListener(delegate { ExpandSection(section); });
+			if (sections.Length > 0)
+				ExpandSection(sections[0]);
+		}
+
+		private void ExpandSection(AccordionSection section)
+		{
+			foreach (AccordionSection s in sections)
+				s.content.SetActive(object.ReferenceEquals(s, section));
+			LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform);
+		}
+	}
+}
---
+++ src/Assets/Scripts/AccordionSection.cs
@@ -0,0 +1,29 @@
+﻿namespace AssemblyCSharp
+{
+	using System;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class AccordionSection : MonoBehaviour
+	{
+		public Button sectionButton;
+		public GameObject content;
+
+		private void Awake()
+		{
+		}
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
