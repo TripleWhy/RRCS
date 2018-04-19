1b28b00984fabedb9a04754dc42176d3e83d96b6
TripleWhy <TripleWhy@users.noreply.github.com>
2018-04-19 19:39:31 +0200

    Added time controls
    Closes #1
    Closes #5

--- src/Assets/Scripts/WordCanvasUi.cs
+++ src/Assets/Scripts/WordCanvasUi.cs
@@ -5,10 +5,55 @@
 	public class WordCanvasUi : MonoBehaviour
 	{
 		public readonly CircuitManager manager = new CircuitManager();
+		private float simuationSpeed = 1f;
+		private bool paused = false;
 
 		void FixedUpdate()
 		{
+			if (paused)
+				Time.timeScale = 0f;
 			manager.EvaluateIfNecessary();
+		}
+
+		public float SimulationSpeed
+		{
+			get
+			{
+				return simuationSpeed;
+			}
+			set
+			{
+				if (value <= 0f)
+					return;
+				simuationSpeed = value;
+				if (Time.timeScale > 0f)
+					Time.timeScale = value;
+			}
+		}
+
+		public void SetSimulationSpeed(string inputString)
+		{
+			float speed;
+			if (float.TryParse(inputString, out speed))
+				SimulationSpeed = speed;
+		}
+
+		public void SimulationPlay()
+		{
+			paused = false;
+			Time.timeScale = SimulationSpeed;
+		}
+
+		public void SimulationPause()
+		{
+			paused = true;
+			Time.timeScale = 0f;
+		}
+
+		public void SimulationStep()
+		{
+			paused = true;
+			Time.timeScale = 1;
 		}
 	}
 }
