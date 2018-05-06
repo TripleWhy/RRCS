b69df1486db3b7c594917a5de9f1fd026d6a24e6
TripleWhy <TripleWhy@users.noreply.github.com>
2018-05-06 21:29:46 +0200

    Added Rng Chip

---
+++ src/Assets/Scripts/circuits/misc/RandomChip.cs
@@ -0,0 +1,35 @@
+﻿namespace AssemblyCSharp
+{
+	using System;
+
+	public class RandomChip : Chip
+	{
+		private static Random random = new Random();
+
+		public RandomChip(CircuitManager manager) : base(manager, 3, 1, false)
+		{
+		}
+
+		override public int IconIndex
+		{
+			get
+			{
+				return 27;
+			}
+		}
+
+		override protected void EvaluateOutputs()
+		{
+			if (ToBool(inputPorts[0]))
+			{
+				try
+				{
+					outputPorts[0].Value = random.Next(ToInt(inputPorts[1]), ToInt(inputPorts[2]));
+				}
+				catch (ArgumentOutOfRangeException)
+				{
+				}
+			}
+		}
+	}
+}
