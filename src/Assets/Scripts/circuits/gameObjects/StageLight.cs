60d89f7a0a3c431d7ae6c260f13052c27d054f4e
TripleWhy <TripleWhy@users.noreply.github.com>
2018-05-17 22:24:02 +0200

    Minor correction

--- src/Assets/Scripts/circuits/gameObjects/StageLight.cs
+++ src/Assets/Scripts/circuits/gameObjects/StageLight.cs
@@ -37,41 +37,41 @@
 				{
 					default:
 					case 0:
-						SetColor(255, 222, 197, (byte)(InValue(2) * 169 / 100));
+						SetColor(255, 222, 197, (byte)(alpha * 169 / 100));
 						break;
 					case 1:
-						SetColor(255,  53,  58, (byte)(InValue(2) * 241 / 100));
+						SetColor(255,  53,  58, (byte)(alpha * 241 / 100));
 						break;
 					case 2:
-						SetColor(255, 127,  66, (byte)(InValue(2) * 241 / 100));
+						SetColor(255, 127,  66, (byte)(alpha * 241 / 100));
 						break;
 					case 3:
-						SetColor(255, 252,  60, (byte)(InValue(2) * 239 / 100));
+						SetColor(255, 252,  60, (byte)(alpha * 239 / 100));
 						break;
 					case 4:
-						SetColor(207, 255, 106, (byte)(InValue(2) * 222 / 100));
+						SetColor(207, 255, 106, (byte)(alpha * 222 / 100));
 						break;
 					case 5:
-						SetColor( 52, 143, 255, (byte)(InValue(2) * 233 / 100));
+						SetColor( 52, 143, 255, (byte)(alpha * 233 / 100));
 						break;
 					case 6:
-						SetColor( 33, 235, 255, (byte)(InValue(2) * 230 / 100));
+						SetColor( 33, 235, 255, (byte)(alpha * 230 / 100));
 						break;
 					case 7:
-						SetColor(  8, 255, 209, (byte)(InValue(2) * 191 / 100));
+						SetColor(  8, 255, 209, (byte)(alpha * 191 / 100));
 						break;
 					case 8:
-						SetColor(255, 158, 233, (byte)(InValue(2) * 242 / 100));
+						SetColor(255, 158, 233, (byte)(alpha * 242 / 100));
 						break;
 					case 9:
-						SetColor(255,  65, 104, (byte)(InValue(2) * 242 / 100));
+						SetColor(255,  65, 104, (byte)(alpha * 242 / 100));
 						break;
 					case 10:
-						SetColor(255, 177, 119, (byte)(InValue(2) * 241 / 100));
+						SetColor(255, 177, 119, (byte)(alpha * 241 / 100));
 						break;
 					case 11:
-						//SetColor(255, 251, 247, (byte)(InValue(2) * 241 / 100)); //Closer to original, but almost invisible on the white background.
-						SetColor(245, 245, 237, (byte)(InValue(2) * 255 / 100));
+						//SetColor(255, 251, 247, (byte)(alpha * 241 / 100)); //Closer to original, but almost invisible on the white background.
+						SetColor(245, 245, 237, (byte)(alpha * 255 / 100));
 						break;
 				}
 			}
