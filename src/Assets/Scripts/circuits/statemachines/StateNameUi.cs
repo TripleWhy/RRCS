0ed686336ae9b69db71723dea891ff35123846b9
_seb <sebastian.barton@yahoo.de>
2019-03-23 20:34:28 +0100

    Add StateNameUi

---
+++ src/Assets/Scripts/circuits/statemachines/StateNameUi.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AssemblyCSharp
+{
+    public class StateNameUi : MonoBehaviour
+    {
+        public Text text;
+        private StateChip chip;
+
+        private void Start()
+        {
+            chip = (StateChip) GetComponent<ChipUi>().Node;
+        }
+
+        private void Update()
+        {
+            text.text = (string) chip.settings[4].currentValue;
+        }
+    }
+}
