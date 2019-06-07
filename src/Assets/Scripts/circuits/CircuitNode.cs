752c12b28ae1b69a5dd6d973885f653f4992965c
_seb <sebastian.barton@yahoo.de>
2019-06-07 20:15:54 +0200

    Revert the fix that didnt fix to fix the fix

--- src/Assets/Scripts/circuits/CircuitNode.cs
+++ src/Assets/Scripts/circuits/CircuitNode.cs
@@ -96,8 +96,6 @@
 				port.Destroy();
 			foreach (InputPort port in inputPorts)
 				port.Destroy();
-			if (statePort != null)
-				statePort.Destroy();
 			if (manager != null)
 				manager.RemoveNode(this);
 		}
