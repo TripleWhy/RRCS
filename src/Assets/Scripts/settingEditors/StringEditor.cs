4a7afdb04c2095e7ea20719d46a605a2f80e6472
_seb <sebastian.barton@yahoo.de>
2019-06-08 23:23:02 +0200

    Fix String Editor not initializing with value

--- src/Assets/Scripts/settingEditors/StringEditor.cs
+++ src/Assets/Scripts/settingEditors/StringEditor.cs
@@ -1,71 +1,80 @@
 ﻿namespace AssemblyCSharp
 {
-	using System;
-	using UnityEngine;
-	using UnityEngine.Events;
-	using UnityEngine.EventSystems;
-	using UnityEngine.UI;
-	using UnityEngine.UI.Extensions;
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+    using UnityEngine.UI.Extensions;
 
-	public class StringEditor : MonoBehaviour
-	{
-		[System.Serializable]
-		public class ValueChangeEvent : UnityEvent<string>
-		{
-		}
+    public class StringEditor : MonoBehaviour
+    {
+        [System.Serializable]
+        public class ValueChangeEvent : UnityEvent<string>
+        {
+        }
 
-		public delegate void ValueChangedEventHandler(StringEditor sender, string value);
+        public delegate void ValueChangedEventHandler(StringEditor sender, string value);
 
-		public event ValueChangedEventHandler ValueChanged = delegate { };
-		public ValueChangeEvent onValueChanged;
+        public event ValueChangedEventHandler ValueChanged = delegate { };
+        public ValueChangeEvent onValueChanged;
 
-		[SerializeField] private string Value = "";
-		private NodeSetting setting;
-		private Text settingNameText;
-		private InputField input;
+        [SerializeField]
+        private string currentValue = "";
+        private NodeSetting setting;
+        private Text settingNameText;
+        private InputField input;
 
-		void Awake()
-		{
-			foreach (Transform child in transform)
-			{
-				if (settingNameText == null)
-					settingNameText = child.GetComponent<Text>();
-				else if (input == null)
-					input = child.GetComponent<InputField>();
-			}
+        void Awake()
+        {
+            foreach (Transform child in transform)
+            {
+                if (settingNameText == null)
+                    settingNameText = child.GetComponent<Text>();
+                else if (input == null)
+                    input = child.GetComponent<InputField>();
+            }
 
-			Debug.Assert(input != null);
+            Debug.Assert(input != null);
 
-			input.text = Value;
+            input.text = Value;
 
-			input.onValueChanged.AddListener(OnInputChanged);
-		}
+            input.onValueChanged.AddListener(OnInputChanged);
+        }
 
-		void Start()
-		{
-			if (onValueChanged == null)
-				onValueChanged = new ValueChangeEvent();
-		}
+        void Start()
+        {
+            if (onValueChanged == null)
+                onValueChanged = new ValueChangeEvent();
+        }
 
-		private void OnInputChanged(string text)
-		{
-			Value = text;
-			ValueChanged(this, text);
-			onValueChanged.Invoke(text);
-		}
+        private void OnInputChanged(string text)
+        {
+            Value = text;
+            ValueChanged(this, text);
+            onValueChanged.Invoke(text);
+        }
 
-		public NodeSetting Setting
-		{
-			get
-			{
-				return setting;
-			}
-			set
-			{
-				settingNameText.text = value.displayName;
-				Value = (string) value.currentValue;
-				setting = value;
-			}
-		}
-	}
+        
+        public string Value
+        {
+            get { return currentValue; }
+            set
+            {
+                currentValue = value;
+                input.text = value;
+            }
+        }
+
+        public NodeSetting Setting
+        {
+            get { return setting; }
+            set
+            {
+                settingNameText.text = value.displayName;
+                Value = (string) value.currentValue;
+                setting = value;
+            }
+        }
+    }
 }
