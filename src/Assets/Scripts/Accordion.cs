a1c862662e00b1ce18cf04fbbc111806b7d0ff1f
_seb <_seb>
2019-06-09 18:42:36 +0200

    Initialize delayed to make sure all the chips are loaded.

--- src/Assets/Scripts/Accordion.cs
+++ src/Assets/Scripts/Accordion.cs
@@ -1,29 +1,39 @@
-﻿namespace AssemblyCSharp
+﻿using System.Collections;
+
+namespace AssemblyCSharp
 {
-	using UnityEngine;
-	using UnityEngine.UI;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-	public class Accordion : MonoBehaviour
-	{
-		public AccordionSection[] sections;
+    public class Accordion : MonoBehaviour
+    {
+        public AccordionSection[] sections;
 
-		void Start()
-		{
-			int i = 0;
-			foreach (AccordionSection section in sections)
-			{
-				section.sectionButton.onClick.AddListener(delegate { ExpandSection(section); });
-				section.transform.SetSiblingIndex(i++);
-			}
-			if (sections.Length > 0)
-				ExpandSection(sections[0]);
-		}
+        void Start()
+        {
+            // Run delayed to make sure all the chips are loaded
+            StartCoroutine(InitializeSections());
+        }
 
-		private void ExpandSection(AccordionSection section)
-		{
-			foreach (AccordionSection s in sections)
-				s.content.SetActive(object.ReferenceEquals(s, section));
-			LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform);
-		}
-	}
+        IEnumerator InitializeSections()
+        {
+            yield return 0;
+            int i = 0;
+            foreach (AccordionSection section in sections)
+            {
+                section.sectionButton.onClick.AddListener(delegate { ExpandSection(section); });
+                section.transform.SetSiblingIndex(i++);
+            }
+
+            if (sections.Length > 0)
+                ExpandSection(sections[0]);
+        }
+
+        private void ExpandSection(AccordionSection section)
+        {
+            foreach (AccordionSection s in sections)
+                s.content.SetActive(object.ReferenceEquals(s, section));
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+        }
+    }
 }
