e7a6224a294a3a52707b335ad458cc1841917ed0
_seb <_seb>
2019-06-14 13:23:37 +0200

    Add signs

---
+++ src/Assets/Scripts/circuits/gameObjects/Sign.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+    public class Sign : CircuitNode
+    {
+        public delegate void TextChangedEventHandler(string message);
+
+        public event TextChangedEventHandler TextChanged = delegate { };
+
+        public Sign(CircuitManager manager) : base(manager, 4, 0, false)
+        {
+        }
+
+        protected override NodeSetting[] CreateSettings()
+        {
+            return new NodeSetting[]
+            {
+                NodeSetting.CreateSetting(NodeSetting.SettingType.Message0),
+                NodeSetting.CreateSetting(NodeSetting.SettingType.Message1),
+                NodeSetting.CreateSetting(NodeSetting.SettingType.Message2),
+                NodeSetting.CreateSetting(NodeSetting.SettingType.Message3),
+                NodeSetting.CreateSetting(NodeSetting.SettingType.Message4),
+                NodeSetting.CreateSetting(NodeSetting.SettingType.LimitMessage),
+            };
+        }
+
+        protected override void EvaluateOutputs()
+        {
+            string message = (string) settings[Math.Max(0, Math.Min(4, inputPorts[3].GetValue()))].currentValue;
+
+            bool limit = (bool) settings[5].currentValue;
+            
+            if (limit)
+                message = message.Substring(0, Math.Min(message.Length, 20));
+
+            message = message.Replace("{R}", inputPorts[0].GetValue().ToString());
+            message = message.Replace("{G}", inputPorts[1].GetValue().ToString());
+            message = message.Replace("{B}", inputPorts[2].GetValue().ToString());
+
+            if (limit)
+                message = message.Substring(0, Math.Min(message.Length, 20));
+
+            TextChanged(message);
+        }
+    }
+}
---
+++ src/Assets/Scripts/ui/SignUi.cs
@@ -0,0 +1,55 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+    public class SignUi : NodeUi
+    {
+        public TextMeshProUGUI text;
+        private Sign sign;
+        public PortUi[] portsUis;
+
+        void Start()
+        {
+            inPorts = new PortUi[4];
+            outPorts = new PortUi[0];
+            statePorts = new PortUi[0];
+
+
+            int i = 0;
+            foreach (PortUi portUi in portsUis)
+            {
+                portUi.nodeUi = this;
+                portUi.Port = Node.inputPorts[i];
+                portUi.PortIndex = i;
+
+                inPorts[i] = portUi;
+
+                i++;
+            }
+        }
+
+        protected override CircuitNode CreateNode(CircuitManager manager)
+        {
+            sign = new Sign(manager);
+            sign.TextChanged += Sign_TextChanged;
+            return sign;
+        }
+
+        private void Sign_TextChanged(string message)
+        {
+            text.text = message;
+        }
+
+        protected override void OnMovedToWorld()
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        protected new void OnDestroy()
+        {
+            sign.TextChanged -= Sign_TextChanged;
+            base.OnDestroy();
+        }
+    }
+}
