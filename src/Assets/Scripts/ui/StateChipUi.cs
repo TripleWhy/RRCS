3d376caaa2c5a80b326989ea0af1b35f6643ffac
TripleWhy <TripleWhy@users.noreply.github.com>
2019-08-19 20:30:07 +0200

    Only update State UI when necessary

---
+++ src/Assets/Scripts/ui/StateChipUi.cs
@@ -0,0 +1,30 @@
+﻿namespace AssemblyCSharp
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class StateChipUi : MonoBehaviour
+	{
+		public Text stateNameText;
+		public Image stateIconImage;
+
+		private void Start()
+		{
+			StateChip chip = (StateChip)GetComponent<ChipUi>().Node;
+			chip.StateNameChanged += Chip_StateNameChanged;
+			Chip_StateNameChanged(chip, chip.StateName);
+			chip.StateActiveChanged += Chip_StateActiveChanged;
+			Chip_StateActiveChanged(chip, chip.Active);
+		}
+
+		private void Chip_StateNameChanged(StateChip source, string stateName)
+		{
+			stateNameText.text = stateName;
+		}
+
+		private void Chip_StateActiveChanged(StateChip source, bool stateActive)
+		{
+			stateIconImage.color = stateActive ? Color.white : Color.gray;
+		}
+	}
+}
