TripleWhy/RRCS

commit f7a1273b44067460dfc52c92b29ead02cb07d8db

State Machine cleanup 2

src/Assets/Scripts/circuits/statemachines/StateChip.cs
@@ -5,7 +5,6 @@
 	public class StateChip : Chip
 	{
 		private bool isActive = false;
-		public bool prevActive = false;
 
 		public delegate void StateNameChangedEventHandler(StateChip source, string stateName);
 		public event StateNameChangedEventHandler StateNameChanged = delegate { };
@@ -29,21 +28,20 @@
 		public override void Evaluate()
 		{
 			EvaluateOutputs();
-
-			prevActive = isActive;
 		}
 
 		protected override void EvaluateOutputs()
 		{
-			outputPorts[0].Value = outputPorts[1].Value = outputPorts[2].Value = 0;
+			bool wasActive = isActive;
 
+			outputPorts[0].Value = outputPorts[1].Value = outputPorts[2].Value = 0;
 			if (isActive)
 			{
 				outputPorts[1].Value = 1;
-				if (!prevActive)
+				if (!wasActive)
 					outputPorts[0].Value = 1;
 			}
-			else if (prevActive)
+			else if (wasActive)
 			{
 				outputPorts[2].Value = 1;
 			}

src/Assets/Scripts/circuits/statemachines/StateMachineChip.cs
@@ -5,9 +5,7 @@
 	public class StateMachineChip : Chip
 	{
 		private int timeInState = 0;
-
 		private StateChip activeState;
-		private StateChip prevActiveState;
 
 		public StateMachineChip(CircuitManager manager)
 			: base(manager, 1, 5, true, Port.PortType.StateRoot)
@@ -58,7 +56,7 @@
 			}
 			else if (IsActive)
 			{
-				prevActiveState = activeState;
+				StateChip lastActiveState = activeState;
 				activeState = FindActiveState();
 
 				timeInState++;
@@ -79,6 +77,7 @@
 				}
 
 				EvaluateOutputs();
+				outputPorts[4].Value = object.ReferenceEquals(lastActiveState, activeState) ? 0 : 1;
 
 				EmitEvaluationRequired();
 			}
@@ -102,8 +101,6 @@
 				outputPorts[2].Value = 0;
 				outputPorts[3].Value = 0;
 			}
-
-			outputPorts[4].Value = prevActiveState != activeState ? 1 : 0;
 		}
 
 		private StateChip FindActiveState()

commit 34fb0006aab195a5224226854a0a639adc7894b3

Fix on enter and on exit pins on state chips that broke with f7a1273b44067460dfc52c92b29ead02cb07d8db

src/Assets/Scripts/circuits/statemachines/StateChip.cs
@@ -5,6 +5,7 @@
 	public class StateChip : Chip
 	{
 		private bool isActive = false;
+		private bool lastWasActive = false;
 		private StateMachineChip stateMachine;
 
 		public delegate void StateNameChangedEventHandler(StateChip source, string stateName);
@@ -33,19 +34,18 @@
 
 		protected override void EvaluateOutputs()
 		{
-			bool wasActive = isActive;
-
 			outputPorts[0].Value = outputPorts[1].Value = outputPorts[2].Value = 0;
 			if (isActive)
 			{
 				outputPorts[1].Value = 1;
-				if (!wasActive)
+				if (!lastWasActive)
 					outputPorts[0].Value = 1;
 			}
-			else if (wasActive)
+			else if (lastWasActive)
 			{
 				outputPorts[2].Value = 1;
 			}
+			lastWasActive = isActive;
 		}
 
 		override protected NodeSetting[] CreateSettings()