ed0433f2e5b812ffd454f8764700f3e1e4dfd260
TripleWhy <TripleWhy@users.noreply.github.com>
2019-09-14 20:04:17 +0200

    Updated default input values for gizmos

--- src/Assets/Scripts/circuits/gizmos/Gizmo.cs
+++ src/Assets/Scripts/circuits/gizmos/Gizmo.cs
@@ -9,6 +9,20 @@
 		{
 		}
 
+		protected override IConvertible DefaultInputValue(int inputIndex)
+		{
+			switch (inputIndex)
+			{
+				case 0:
+					return false;
+				case 1:
+				case 2:
+					return 0F;
+				default:
+					return base.DefaultInputValue(inputIndex);
+			}
+		}
+
 		public abstract string GetGizmoValueString();
 
 		public abstract void ResetGizmo();
