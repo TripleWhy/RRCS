69e6f36bcc09f6ef65d87f00afba7b063ac96657
_seb <_seb>
2020-02-14 18:30:55 +0100

    Also hide scrollbars if container is invisible Start with sidebars hidden on mobile

--- src/Assets/Scripts/LayoutManager.cs
+++ src/Assets/Scripts/LayoutManager.cs
@@ -9,14 +9,27 @@ namespace AssemblyCSharp
 	{
 		public LayoutElement leftSidebar;
 		public LayoutElement rightSidebar;
+		public LayoutElement leftSidebarScrollbar;
+		public LayoutElement rightSidebarScrollbar;
+		public Toggle sidebarToggle;
 
 		private float leftSidebarInitialWidth = 0;
 		private float rightSidebarInitialWidth = 0;
-		
+		private float leftSidebarScrollbarInitialWidth = 0;
+		private float rightSidebarScrollbarInitialWidth = 0;
+
 		public void Start()
 		{
 			leftSidebarInitialWidth = leftSidebar.preferredWidth;
 			rightSidebarInitialWidth = rightSidebar.preferredWidth;
+			leftSidebarScrollbarInitialWidth = leftSidebarScrollbar.preferredWidth;
+			rightSidebarScrollbarInitialWidth = rightSidebarScrollbar.preferredWidth;
+
+			if (Screen.width < 600)
+			{
+				// Hide Sidebars on small screens
+				sidebarToggle.isOn = false;
+			}
 		}
 
 		public void SetSidebarsVisible(bool visible)
@@ -25,11 +38,15 @@ namespace AssemblyCSharp
 			{
 				leftSidebar.preferredWidth = leftSidebarInitialWidth;
 				rightSidebar.preferredWidth = rightSidebarInitialWidth;
+				leftSidebarScrollbar.preferredWidth = leftSidebarScrollbarInitialWidth;
+				rightSidebarScrollbar.preferredWidth = rightSidebarScrollbarInitialWidth;
 			}
 			else
 			{
 				leftSidebar.preferredWidth = 0;
 				rightSidebar.preferredWidth = 0;
+				leftSidebarScrollbar.preferredWidth = 0;
+				rightSidebarScrollbar.preferredWidth = 0;
 			}
 		}
 	}
