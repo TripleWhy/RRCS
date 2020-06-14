164e75f7c16eec00fc3fa1eccc8c760964b2ef53
_seb <_seb>
2020-06-14 14:41:36 +0200

    Add runtimeConfig template

---
+++ src/Assets/WebGLTemplates/web/runtimeConfig.js
@@ -0,0 +1,6 @@
+// Default runtime config
+window.runtimeConfig = {
+	BLOB_URL: "https://rrcs.tk/rrcs-blob/",
+	SHARE_URL_BASE: "https://rrcs.tk/circuit/",
+	STATE_REPLACE_BASE: "/RRCS/?circuit="
+};
